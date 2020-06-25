6b6f61e9b904181b4044d22565165597971f3507
mark <mark@rgames.jp>
2020-06-25 09:54:06 +0900

    Updates

--- main.212cf.js
+++ main.212cf.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f2935.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.666f2.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
